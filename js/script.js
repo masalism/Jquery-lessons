c39ee3dacbde5a64b95a5bf092db08036deb99ac
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-08-04 22:55:57 +0300

    b

--- js/script.js
+++ js/script.js
@@ -42,4 +42,48 @@ $(function () {
   $("p").slideDown(2000);
   */
 
+  // MOVING ELEMENTS //
+
+  /*
+  $(".blue-box").animate({
+    "margin-left": "+=200px"
+  }, 1000, "swing"); //time
+  */
+
+  // CUSTOM ANIMATION //
+
+  /*
+  //can't use colors
+  $(".blue-box").animate({
+    marginLeft: "200px",
+    opacity : "0",
+    "height" : "50px",
+    "width" : "50px",
+    marginTop : "25px"
+  }, 1000);
+
+  $("p").animate({
+    fontSize: "20px"
+  }, 1000);
+  */
+
+  // DELAYING AND CHAINING //
+
+  /*
+  $(".red-box").fadeTo(1000, 0.2);
+  $(".green-box").delay(1000).fadeTo(1000, 0.5);
+  $(".blue-box").delay(2000).fadeTo(1000, 0.8).fadeOut().delay(500).fadeIn();
+  */
+
+  // TIMING ANIMATIONS USING CALLBACK FUNCTIONS //
+
+  /*
+  $(".red-box").fadeTo(1000, 0, function() {
+    $(".green-box").fadeTo(1000, 0, function() {
+      $(".blue-box").fadeTo(1000, 0);
+    });
+  });
+  */
+
+  
 });
