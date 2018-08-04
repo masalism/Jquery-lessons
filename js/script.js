db14562aac0c023f2803c8d97f00f58126622ba5
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-08-04 11:41:26 +0300

    fade in fade out

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-$(document).ready(function () {
-    // jQuery goes here...
-  
-    // Uncomment this line to fade out the red box on page load
-    $(".red-box").fadeOut(2000);
+$(function () {
+  // jQuery goes here...
+  /*
+  $(".red-box").fadeOut(1000);
+  $(".green-box").fadeOut(1000);
+  $(".blue-box").fadeOut(1000);
+  $(".red-box").fadeIn(1000);
+  $(".green-box").fadeIn(1000);
+  $(".blue-box").fadeIn(1000);
+  $(".red-box").fadeTo(3000, 0.2); //time, opacity
+  $(".green-box").fadeTo(2000, 0.5);
+  $(".blue-box").fadeTo(1000, 0.8);
+
+  $(".blue-box").fadeToggle(); //memorizes opacity
+  $(".blue-box").fadeToggle();
+  */
+
+  $(".green-box").fadeOut(1000);
+  $(".green-box").fadeTo(1000, 0.5);
 });
