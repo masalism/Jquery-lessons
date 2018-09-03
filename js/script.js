6642093ef6e1e7bdf962dc0f9f51f3432b2e3d5f
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-09-03 12:01:03 +0300

    modularizing

--- js/script.js
+++ js/script.js
@@ -383,5 +383,34 @@ $(function () {
   });
   */
 
-  
+  // MODULARIZING EVENTS HANDLERS // 
+
+  /*
+  function logEvent() {
+    console.log("Mouse or key was clicked");
+  }
+
+  $("html").on("click keydown", logEvent);
+
+  var images = [
+    "/images/laptop-mobile_small.jpg",
+    "/images/laptop-on-table_small.jpg",
+    "/images/people-office-group-team_small.jpg"
+  ];
+
+  var i = 0;
+  var galleryImage = $(".gallery").find("img");
+  galleryImage.on("click", switchToNextImage);
+
+  function switchToNextImage() {
+    i = (i + 1) % images.length;
+    galleryImage.fadeOut(function() {
+      galleryImage.attr("src", images[i]).fadeIn();
+    });
+  }
+  */
+
+  // DELEGATED EVENTS //
+
 });
+
