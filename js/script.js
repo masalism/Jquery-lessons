187f40b6bc6848ae1900fd080dda3617e2c89a6c
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-09-03 13:47:43 +0300

    IMAGE GALLERY

--- js/script.js
+++ js/script.js
@@ -412,6 +412,7 @@ $(function () {
 
   // DELEGATED EVENTS //
 
+  /*
   // $("p").click(function() {
   //   $(this).slideUp();
   // });
@@ -428,6 +429,51 @@ $(function () {
   $("body").on("mouseenter", "li", function() {
     $(this).css("color", "#666");
   });
+  */
+
+  //PASSING ADDITIONAL DATA TO EVENTS //
+
+  /*
+  $("#btn-click").click({
+    user: "Peter",
+    domain: "petersommerhof.com"
+  }, function(event) {
+    greetUser(event.data);
+  });
+
+  function greetUser(userdata) {
+    username = userdata.user || "Anonymous";
+    domain = userdata.domain || "example.com";
+    alert("Welcome back " + username + " from " + domain);
+  }
+  */
+
+  // CREATING AN IMAGE GALLERY //
+
+  /*
+  var galleryItems = $(".gallery").find("img");
+  galleryItems.css("width", "33%").css("opacity", "0.7");
+
+  galleryItems.mouseenter(function () {
+    $(this).stop().fadeTo(500, 1);
+  });
+  
+  galleryItems.mouseleave(function () {
+    $(this).stop().fadeTo(500, 0.7);
+  });
+
+  galleryItems.click(function() {
+    var source = $(this).attr("src");
+    var image = $("<img>").attr("src", source).css("width", "100%");
+    $(".lightbox").empty().append(image).fadeIn(2000);
+  });
+
+  $(".lightbox").click(function() {
+    $(this).stop().fadeOut();
+  })
+  */
+
+  // HANDLING KEYDOWN AND KEYUP EVENTS //
 
 });
 
