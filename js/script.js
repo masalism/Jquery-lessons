5fe9e61268399ef7c59ac5e2ebe6cf267f119b26
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-09-04 11:48:47 +0300

    pokemon api

--- js/script.js
+++ js/script.js
@@ -14,6 +14,7 @@ $(function() {
 
   // RETRIEVING FLICKR IMAGES TROUGHTHE FLICKR API //
 
+  /*
   var flickrApiUrl = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
 
   $.getJSON(flickrApiUrl, {
@@ -35,6 +36,29 @@ $(function() {
   }).fail(function () {
     alert("AJAX call failed");
   });
-   
+  */
+
+  // CODING ACTIVITY: RETRIEVING POKEMON DATA FROM THEPOKEAPI //
+
+  var pokeapi = "https://pokeapi.co/api/v2/generation/1";
+
+  $.getJSON(pokeapi).done(function(data) {
+    console.log(data);  // Explore available data in the Developer Tools Console
+    $.each(data.pokemon_species, function(index, pokemon) {
+      var name = capitalize(pokemon.name);
+      var paragraph = $("<p>").html("Pokémon species no. " + (index+1) + " is " + name);
+      paragraph.appendTo("#pokedex");
+    });
+  }).fail(function() {
+    // Handle error case
+    console.log("Call to PokéAPI failed.");
+  }).always(function() {
+    // Will always be executed (in either case).
+    console.log("Pokémon is awesome.")
+  });
 
 });
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
